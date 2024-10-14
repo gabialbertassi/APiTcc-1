df0e94484567d0fc16631e94166cb063cc132395
JSGomes2 <Sgomes.julia213@gmail.com>
2024-10-13 22:14:30 -0300

    endpois feitos ( falta so consultar por id)

--- src/controller/ProdutosController.js
+++ src/controller/ProdutosController.js
@@ -16,4 +16,68 @@ endpoints.post('/produtos/',async (req, resp) => {
         })
     }
 })
+
+
+
+
+endpoints.get('/produtos/', async (req, resp) => {
+        try {
+                let registros =await db.ConsultarProdutos();
+                        resp.send (registros);
+
+                            } catch (err) {
+                                    resp.status(400).send({
+                                                erro: err.message
+                                                        })
+                                                            }
+
+
+                                                            })
+
+
+
+
+endpoints.put('/produtos/:id',async (req, resp) => {
+        try {
+           let id= req.params.id;
+                   let produto= req.body;
+                   let linhasAfetadas= await db.alterarProduto(id,produto);
+                   if(linhasAfetadas>=1){
+                   resp.send();
+                   }
+                       else{resp.status(404).send({erro: 'nenhum registro encontrado'})}
+                           } catch (err) {
+                                   resp.status(400).send({
+                                               erro: err.message
+                                                       })
+                                                           }
+                                                           })
+                                                            
+endpoints.delete('/produtos/:id', async (req,resp) => {
+
+try{
+let id = req.params.id;
+
+let linhasAfetadas = await db.removerProduto(id)
+
+if(linhasAfetadas>=1){
+resp.send();
+}
+    else{resp.status(404).send({erro: 'nenhum registro encontrado'})}
+        } catch (err) {
+                resp.status(400).send({
+                            erro: err.message
+                                    })
+
+
+                                    }
+
+
+                                    })
+
+                                                        
+
+
+
+
 export default endpoints
