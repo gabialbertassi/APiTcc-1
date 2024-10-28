123e592b547f623cb73bb619a39d6ac182be863c
gabialbertassi <gabialbertassi01@gmail.com>
2024-10-28 16:00:01 -0300

    recuperação de senha

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,6 +1,6 @@
 import * as db from '../repository/loginRepository.js';
-
-
+import axios from 'axios';
+import nodemailer from 'nodemailer'
 import  {Router} from "express";
 import { autenticar, gerarToken } from '../utils/jwt.js';
 
@@ -49,5 +49,137 @@ endpoints.post('/entrar/',  async (req,resp) =>{
 
 
 })
+
+
+
+
+endpoints.post('/verificar-email', async (req, res) => {
+    const { email } = req.body;
+    const apiKey = 'ae09e7fe7b69cd42090c327a40c6604e4fcacf91'; 
+    const url = `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Erro ao verifDicar e-mail:', error);
+        res.status(500).json({ error: 'Erro ao verificar e-mail' });
+    }
+});
+
+
+
+
+endpoints.post('/verificar-email2', async (req, resp) => {
+    try {
+        const { email } = req.body;
+
+        
+        const existe = await db.verificarEmail(email);
+
+        if (existe) {
+            const codigo = Math.floor(100000 + Math.random() * 900000); 
+            
+           
+
+            await enviarEmail(email, codigo);
+
+            let a = await db.cadastrarCodigo(codigo, email);
+            
+            resp.send({ existe: true, codigo }); 
+        } else {
+            resp.send({ existe: false });
+        }
+    } catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        });
+    }
+});
+
+
+endpoints.post('/redefinir-senha', async (req, resp) => {
+    try {
+        const { novaSenha, email, codigo } = req.body;
+
+        const resultado = await db.redefinirSenha(novaSenha, email, codigo);
+
+        if (resultado) {
+            resp.send({ success: true, message: 'Senha redefinida com sucesso!' });
+        } else {
+            resp.status(400).send({ success: false, message: 'Erro ao redefinir a senha. Verifique o e-mail.' });
+        }
+    } catch (err) {
+        console.error('Erro ao redefinir a senha:', err);
+        resp.status(500).send({ error: err.message });
+    }
+});
+
+
+
+endpoints.put('/usuarios/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nome } = req.body;
+
+    if (!nome) {
+        return res.status(400).send('O campo  é obrigatório');
+    }
+
+    try {
+        const resultado = await db.AlterarNome(id, { nome });
+
+        if (resultado > 0) {
+            res.status(200).send({ message: 'Nome alterado com sucesso' });
+        } else {
+            res.status(404).send({ message: 'Usuário não encontrado' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro interno do servidor');
+    }
+});
+
+
+
+
+
+
+
+
+async function enviarEmail(email, codigo) {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'ra44567415892@acaonsfatima.org.br', 
+            pass: 'FREI20052426'
+        }
+    });
+    const mailOptions = {
+        from: 'ra44567415892@acaonsfatima.org.br',
+        to: email,
+        subject: 'Código de Redefinição de Senha',
+        html: `
+        <div style="font-family: Arial, sans-serif; background-color: #f7f7f7; padding: 20px; border-radius: 5px;">
+            <div style="max-width: 600px; margin: auto; background-color: white; padding: 20px; border-radius: 5px; box-shadow: 0 2px 10px rgba(0,0,0,0.1);">
+                <h2 style="color: #6A0DAD;">Redefinição de Senha</h2>
+                <p style="color: #333;">Olá,</p>
+                <p style="color: #333;">Você solicitou a redefinição da sua senha. Use o código abaixo para prosseguir:</p>
+                <h3 style="color: #6A0DAD; font-size: 24px;">${codigo}</h3>
+                <p style="color: #333;">Se você não solicitou essa mudança, pode ignorar este email.</p>
+                <hr style="border: 1px solid #6A0DAD;">
+                <footer style="text-align: center; color: #777;">
+                    <p>&copy; ${new Date().getFullYear()} Sua Empresa. Todos os direitos reservados.</p>
+                </footer>
+            </div>
+        </div>
+    `
+    };
+
+    await transporter.sendMail(mailOptions);
+}
+   
+
 
 export default endpoints
--- src/repository/loginRepository.js
+++ src/repository/loginRepository.js
@@ -31,3 +31,46 @@ export async function validarUsuario(pessoa){
     return registros[0][0]
 
 }
+
+
+export async function verificarUsuarioExistente(email, telefone) {
+    const comando = `
+        SELECT * FROM tb_usuario 
+        WHERE nm_usuario = ?
+    `;
+    let registros = await con.query(comando, [email, telefone]);
+    return registros[0]; 
+}
+
+export async function verificarEmail(email) {
+    const comando = `
+        SELECT nm_usuario FROM tb_usuario 
+        WHERE nm_usuario = ?
+    `;
+    let registros = await con.query(comando, [email]);
+    return registros[0].length > 0;
+}
+
+export async function redefinirSenha(novaSenha, email, codigo) {
+    const comando = `
+        UPDATE tb_usuario 
+        SET ds_senha = ? 
+        WHERE nm_usuario = ? and codigo = ?
+    `;
+    
+    const resultado = await con.query(comando, [novaSenha, email, codigo]);
+    return resultado[0].affectedRows > 0;
+}
+ 
+export async function cadastrarCodigo(codigo, email){
+
+
+    const comando = `
+    UPDATE tb_usuario  
+    SET codigo = ? 
+    WHERE nm_usuario = ? 
+    `;
+    const resultado = await con.query(comando, [codigo , email]);
+    return resultado[0].affectedRows > 0;
+
+}
